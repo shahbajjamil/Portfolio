0c95062cfc31280f4598aaacc3d405d831341fd4
shahbajjamil <shahbajjamil2014@gmail.com>
2021-11-14 17:10:07 +0530

    project page

--- lib/screens/home/project/project.dart
+++ lib/screens/home/project/project.dart
@@ -1,7 +1,9 @@
 import 'dart:html';
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:portfolio/model/project_model.dart';
 import 'package:portfolio/utils/color.dart';
 import 'package:portfolio/utils/constants.dart';
 import 'package:portfolio/utils/fonts.dart';
@@ -15,10 +17,126 @@ class Project extends StatefulWidget {
 }
 
 class _ProjectState extends State<Project> {
+  final CarouselController _controller = CarouselController();
+  int _current = projectList.isEmpty ? 0 : 1;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-
+    final List<Widget> projectSlider = projectList
+        .map(
+          (item) => Container(
+            // color: AppColor().black,
+            height: size.height - (size.height * 0.1 + padding * 6),
+            child: Stack(
+              children: [
+                Positioned(
+                  bottom: padding * 2,
+                  right: 0,
+                  child: Container(
+                    // width: size.width - padding * 2,
+                    padding: EdgeInsets.fromLTRB(
+                        padding, padding * 2, padding, padding),
+                    decoration: BoxDecoration(
+                      color: AppColor().lightBlue,
+                      borderRadius: BorderRadius.circular(padding),
+                      boxShadow: [
+                        BoxShadow(
+                          color: AppColor().black.withOpacity(0.3),
+                          blurRadius: 10,
+                          offset: Offset(0, 5),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          item.subTitle,
+                          style: sb25_black(),
+                        ),
+                        SizedBox(width: 100),
+                        Text('${_current}', style: sb25_black()),
+                        Text(" / " + projectList.length.toString(),
+                            style: sb25_grey()),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    vertical: doublePadding,
+                    horizontal: doublePadding * 2,
+                  ),
+                  decoration: BoxDecoration(
+                    color: AppColor().lightPink,
+                    borderRadius: BorderRadius.circular(padding),
+                    boxShadow: [
+                      BoxShadow(
+                        color: AppColor().black.withOpacity(0.3),
+                        blurRadius: 10,
+                        offset: Offset(0, 5),
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // image container
+                      Container(
+                        decoration: BoxDecoration(
+                          color: AppColor().lightBlue,
+                          boxShadow: [
+                            BoxShadow(
+                              color: AppColor().black.withOpacity(0.3),
+                              blurRadius: padding,
+                            )
+                          ],
+                          borderRadius: BorderRadius.circular(padding),
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(item.imageUrl),
+                          ),
+                        ),
+                        width: size.width * 0.37,
+                        height: size.height * 0.5,
+                      ),
+                      Container(
+                        width: size.width * 0.37,
+                        height: size.height * 0.5,
+                        // color: AppColor().lightBlue,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(
+                              item.title,
+                              style: sb25_black(),
+                            ),
+                            SizedBox(height: padding),
+                            Text(
+                              item.discription,
+                              style: n20_black(),
+                            ),
+                            SizedBox(height: padding),
+                            // Spacer(),
+                            Button(
+                              text: "Full View",
+                              onClicked: () {
+                                print("full view" + item.projectUrl);
+                              },
+                              textColor: true,
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+        .toList();
     return Container(
       width: double.infinity,
       height: size.height,
@@ -49,105 +167,22 @@ class _ProjectState extends State<Project> {
           SizedBox(height: padding * 2),
           Stack(
             children: [
-              Container(
-                // color: AppColor().black,
-                height: size.height - (size.height * 0.1 + padding * 6),
-                child: Stack(
-                  children: [
-                    Positioned(
-                      bottom: padding * 2,
-                      right: 0,
-                      child: Container(
-                        // width: size.width - padding * 2,
-                        padding: EdgeInsets.fromLTRB(
-                            padding, padding * 2, padding, padding),
-                        decoration: BoxDecoration(
-                          color: AppColor().lightBlue,
-                          borderRadius: BorderRadius.circular(padding),
-                          boxShadow: [
-                            BoxShadow(
-                              color: AppColor().black.withOpacity(0.3),
-                              blurRadius: 10,
-                              offset: Offset(0, 5),
-                            ),
-                          ],
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "shfdfkgdfk fhsfksdf sdsfsd fsdfsdf",
-                              style: sb25_black(),
-                            ),
-                            SizedBox(width: 100),
-                            Text("01", style: sb25_black()),
-                            Text(" / 04", style: sb25_grey()),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.symmetric(
-                        vertical: doublePadding,
-                        horizontal: doublePadding * 2,
-                      ),
-                      decoration: BoxDecoration(
-                        color: AppColor().lightPink,
-                        borderRadius: BorderRadius.circular(padding),
-                        boxShadow: [
-                          BoxShadow(
-                            color: AppColor().black.withOpacity(0.3),
-                            blurRadius: 10,
-                            offset: Offset(0, 5),
-                          ),
-                        ],
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          // image container
-                          Container(
-                            decoration: BoxDecoration(
-                              color: AppColor().red,
-                              borderRadius: BorderRadius.circular(padding),
-                            ),
-                            width: size.width * 0.37,
-                            height: size.height * 0.5,
-                          ),
-                          Container(
-                            width: size.width * 0.37,
-                            height: size.height * 0.5,
-                            // color: AppColor().lightBlue,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "Project Title Name",
-                                  style: sb25_black(),
-                                ),
-                                SizedBox(height: padding),
-                                Text(
-                                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea......",
-                                  style: n20_black(),
-                                ),
-                                SizedBox(height: padding),
-                                // Spacer(),
-                                Button(
-                                  text: "Full View",
-                                  onClicked: () {
-                                    print("full view");
-                                  },
-                                  textColor: true,
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+              CarouselSlider(
+                items: projectSlider,
+                carouselController: _controller,
+                options: CarouselOptions(
+                    height: size.height - (size.height * 0.1 + padding * 6),
+                    enlargeCenterPage: false,
+                    viewportFraction: 1,
+                    disableCenter: false,
+                    autoPlayAnimationDuration: Duration(milliseconds: 2000),
+                    autoPlayCurve: Curves.easeInOut,
+                    autoPlay: true,
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        _current = index + 1;
+                      });
+                    }),
               ),
               Container(
                 // color: AppColor().lightBlue,
@@ -156,11 +191,15 @@ class _ProjectState extends State<Project> {
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
-                    Icon(Icons.chevron_left, size: 50),
+                    InkWell(
+                        onTap: () => _controller.previousPage(),
+                        child: Icon(Icons.chevron_left, size: 50)),
                     RotatedBox(
                         quarterTurns: 1,
                         child: Icon(Icons.horizontal_rule, size: 50)),
-                    Icon(Icons.chevron_right, size: 50),
+                    InkWell(
+                        onTap: () => _controller.nextPage(),
+                        child: Icon(Icons.chevron_right, size: 50)),
                   ],
                 ),
               ),
